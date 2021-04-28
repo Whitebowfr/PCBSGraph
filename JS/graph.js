c66f436f402e5671c0907b5a7d358db0ac03a8af
Whitebowfr <69417022+Whitebowfr@users.noreply.github.com>
2021-04-28 21:26:57 +0200

    Fixed OC score

--- JS/graph.js
+++ JS/graph.js
@@ -162,7 +162,7 @@ function updateGraph(priority, compatibleSort) {
             if (previousType != type) legendBase.push("Score", "Part Ranking")
             break;
         case "cpus":
-            required.push("frequency", "maxFrequency", "defaultMemorySpeed", "price", "wattage", "level", "stv", "basicScore", "partRankingScore")
+            required.push("frequency", "maxFrequency", "defaultMemorySpeed", "price", "wattage", "level", "stv", "basicScore", "ocScore", "partRankingScore")
             if (previousType != type) legendBase.push("Score", "Part Ranking")
             break;
         case "ram":
@@ -194,6 +194,7 @@ function updateGraph(priority, compatibleSort) {
         "defaultMemorySpeed": "Default Mem Freq",
         "wattage": "Watts consumed",
         "basicScore": "Score",
+        "ocScore": "Max Score",
         "partRankingScore": "Part Ranking",
         "baseCoreClock": "Base Core Clock",
         "vram": "VRAM (GB)",
